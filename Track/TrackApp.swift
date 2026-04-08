195f1d4a25f461d368978c5dc107cf048839eb95
oguen <lvve111@proton.me>
2026-04-08 11:20:21 +0200

    Initial Commit

---
+++ Track/TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackApp.swift
+//  Track
+//
+//  Created by Ogün Özdemir on 08.04.26.
+//
+
+import SwiftUI
+
+@main
+struct TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
